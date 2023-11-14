3255912f08b7cc96414e8ffeee5acaff1dca2dd8
thecliffes <93997594+thecliffes@users.noreply.github.com>
2023-11-14 16:09:32 +0000

    Delete Java directory

--- Java/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-	public static void main(String[] args){
-		boolean a = true;
-		boolean b = true;
-		boolean c = false;
-		System.out.println(true && c || (!a ^ b) && !c && 7 < 11);
-	}
-}

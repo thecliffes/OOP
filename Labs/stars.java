848f6a8719bb80393f0506cdacd0c2d3614e120d
thecliffes <93997594+thecliffes@users.noreply.github.com>
2023-11-14 16:14:25 +0000

    Add files via upload

---
+++ Labs/stars.java
@@ -0,0 +1,12 @@
+public class stars {
+	public static void main(String[] args){
+		int i=0;
+		int j=0;
+		for(i=0;i<5;i++){
+			for(j=0;j<8;j++){
+				String temp = temp + "*";
+				System.out.println(temp);
+			}
+		}
+	}
+}
